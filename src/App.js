459081ed0bd0719ddd7d0d23056ecddb7f40fa8f
Deepak lak <dkumar17071@gmail.com>
2023-07-16 19:31:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h1>welcome to the react world</h1>
+      <p>this is also visible</p>
+
+   </div>
+  );
+}
+
+export default App;
