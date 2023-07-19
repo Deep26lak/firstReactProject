1a6d67716b6da88c6ae200ec92d2de2081ef31ea
Deepak lak <dkumar17071@gmail.com>
2023-07-19 22:36:11 +0530

    working with new  components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import "./ExpenseItem.css";
+import Card from  "./Card"
 import ExpenseDate from "./ExpenseDate";
-function ExpenseItem(props) {
-//   const expenseDate = new Date();
-//   const expenseTitle = "Car Insurance";
-//   const expensePrice = 293.43;
 
+
+
+function ExpenseItem(props) {
   return (
-    <div className="expense-item ">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h1 className="expense-item h1">{props.title}</h1>
+        <h1>{props.title}</h1>
         <p className="expense-item__price">${props.amount}</p>
       </div>
-    </div>
+    </Card>
   );
 }
 
