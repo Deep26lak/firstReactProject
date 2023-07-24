40043b011bcb131114d90b2c0e81cf23256f17c8
Deepak lak <dkumar17071@gmail.com>
2023-07-24 11:52:59 +0530

    commit

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import "./ExpenseFilter.css"
-
+import "./ExpenseFilter.css";
 
 const ExpensesFilter = () => {
+  const dropDownChangeHandler = (event) => {
+    const year = event.target.value;
+    console.log(year);
+  };
+
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022' selected>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select onChange={dropDownChangeHandler}>
+          <option value="2022" selected>
+            2022
+          </option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
