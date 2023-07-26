c86037453429650b6136db657cc54f930de83bac
Deepak lak <dkumar17071@gmail.com>
2023-07-26 13:03:59 +0530

    creating dynamic array list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [title,setTitle]=useState(props.title)  // use of useState hook , it alwayes return a array with two values first one is provided value and the second one updated value !!!
 
-
-  const clickhandler = () => {
-    setTitle("updated!!") // seting updated value to second element of use state array
-  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h1>{title}</h1>
+        <h1>{props.title}</h1>
         <p className="expense-item__price">${props.amount}</p>
       </div>
-      <button onClick={clickhandler}>click here</button> 
     </Card>
   );
 };
